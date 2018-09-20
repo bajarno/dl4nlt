ba913055c5ec26e81f23478581827c236ec7984e
Freek <freek.boutkan@gmail.com>
2018-09-20 13:21:01 +0200

    Major datahandeling improvements: * Dynamic padding. Padding in batch to longest sentence (or max length) * Frequency-based dictionary. Most frequent word will be token 1, etc * N-gram support: Extra start tokens, to use set markov_order=N in   get_dataloaders functionality (defaults to 2) * In-batch sorting based on title length
    Minor adjustments:
    * Vim swap files in gitignore
    * Use logging instead of prints
    * Data loader now also returns length of articles and content

--- code/dataloader.py
+++ code/dataloader.py
@@ -1,41 +1,90 @@
 import torch
+from collections import Counter
 from torch.utils.data.sampler import SubsetRandomSampler
 import csv
+import logging
 import numpy as np
 import torch.utils.data
 
+logger = logging.getLogger(__name__)
+
 class TextCSVDataset(torch.utils.data.Dataset):
 
-    def __init__(self, fn):
+    def __init__(self, fn, markov_order=2):
+        if markov_order < 2:
+            raise ValueError('Markov order < 2 is currently not supported')
+
         self.fn = fn
-        self.max_tokens_content = 100
-        self.max_tokens_title = 30 
+        self.order = markov_order
+        self.max_padding_article = 120
+        self.max_padding_title = 40 
         self.read_data()
 
+        logger.info('Vocab size {}'.format(self.vocab_size))
+        logger.info('Lines {}'.format(len(self.titles)))
+        logger.info('Max title length (tokens) {}'.format(max(self.title_lengths)))
+        logger.info('Max article length (tokens) {}'.format(max(self.article_lengths)))
+
+    @property
+    def vocab_size(self):
+        return len(self.w2i) + 1 # to account for padding token
+
     def read_data(self):
+
+        logger.info('Reading dataset %s', self.fn)
         self.w2i = dict()
         self.i2w = dict()
+
         self.titles = []
-        self.content = []
+        self.title_lengths = []
+        self.articles = []
+        self.article_lengths = []
+
+        counts = self.cap_text_and_count_frequencies()
+        self.build_dictionary(counts)
+        self.titles = self.convert_to_indices(self.titles)
+        self.articles = self.convert_to_indices(self.articles)
+
+    def build_dictionary(self, counts):
+        words, _ = zip(*counts.most_common())
+        for index, word in enumerate(words):
+            self.w2i[word] = index + 1
+            self.i2w[index] = word
+
+
+    def convert_to_indices(self, data):
+        for i, datum in enumerate(data):
+            for j, word in enumerate(datum):
+                data[i][j] = self.w2i[word]
+
+        return data
+
+    def cap_text_and_count_frequencies(self):
+        counter = Counter()
 
         with open(self.fn, 'r') as f_in:
             csvreader = csv.reader(f_in)
-            for title, content in csvreader:
-                title = self.add_vocab_return_indices(title, is_title=True)
-                content = self.add_vocab_return_indices(content, is_title=False)
-
+            for title, article in csvreader:
+                title = self.cap_string(title, self.max_padding_title)
+                article = self.cap_string(article, self.max_padding_article)
+                
                 self.titles.append(title)
-                self.content.append(content)
+                self.title_lengths.append(len(title))
+                self.articles.append(article)
+                self.article_lengths.append(len(article))
 
-    def add_vocab_return_indices(self, string, is_title=False):
-        max_tokens = self.max_tokens_title if is_title else self.max_tokens_content
-        string = string.strip().split()[:max_tokens]
-        indices = [0] * max_tokens
-        for i, word in enumerate(string):
-            self.add_word(word)
-            indices[i] = self.w2i[word]
+                for word in title + article:
+                    counter[word] += 1
 
-        return indices
+        return counter
+
+    def cap_string(self, string, max_length):
+        string = string.strip().split()
+        extra_starttags = self.order - 2
+        string = [string[0]] * extra_starttags + string
+        string = string[:max_length]
+
+        return string
 
     def add_word(self, word):
         if word not in self.w2i:
@@ -45,39 +94,61 @@ class TextCSVDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, x):
         title = self.titles[x]
-        content = self.content[x]
-        return torch.LongTensor(title), torch.LongTensor(content)
+        article = self.articles[x]
+        return torch.LongTensor(article), torch.LongTensor(title), self.article_lengths[x], self.title_lengths[x]
+
+    def make_square(self, inputs):
+        n_data = len(inputs)
+        max_length = max([len(item) for item in inputs])
+        square = torch.zeros(n_data, max_length)
+
+        for i, item in enumerate(inputs):
+            square[i, :len(item)] = item
+
+        return square
+
+    def make_batch(self, data):
+        # longest Y first
+        data.sort(key=lambda x: x[3], reverse=True)
+        articles, titles, article_lengths, title_lengths = zip(*data)
+        
+        # dynamic padding
+        articles = self.make_square(articles)
+        titles = self.make_square(titles)
+
+        return articles, titles, torch.tensor(article_lengths), torch.tensor(title_lengths)
 
     def __len__(self):
         return len(self.titles)
 
-def get_dataloaders(fn, batch_size=16, validation_split=.2):
-    dataset = TextCSVDataset(fn)
-    shuffle_dataset = True
-    random_seed = 42
-
+def get_dataloaders(fn, markov_order=2, batch_size=16, validation_split=.2, shuffle_dataset=True, random_seed=42):
     # Creating data indices for training and validation splits:
+    dataset = TextCSVDataset(fn, markov_order)
     dataset_size = len(dataset)
     indices = list(range(dataset_size))
     split = int(np.floor(validation_split * dataset_size))
+
     if shuffle_dataset:
         np.random.seed(random_seed)
         np.random.shuffle(indices)
-        train_indices, val_indices = indices[split:], indices[:split]
 
-        # Creating PT data samplers and loaders:
-        train_sampler = SubsetRandomSampler(train_indices)
-        valid_sampler = SubsetRandomSampler(val_indices)
+    train_indices, val_indices = indices[split:], indices[:split]
 
-        train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler)
-        validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=valid_sampler)
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
 
-        return train_loader, validation_loader
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, collate_fn=dataset.make_batch)
+    validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=valid_sampler, collate_fn=dataset.make_batch)
+
+    return train_loader, validation_loader
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     train_loader, test_loader = get_dataloaders('test/preprocessed_testdata.csv')
 
-    for batch_idx, (titles, contents) in enumerate(train_loader):
-        print(batch_idx, titles.shape, contents.shape)
-        print(titles)
-        break
+    for batch_idx, (articles, titles, article_lengths, title_lengths) in enumerate(train_loader):
+        logger.debug('Print first batch to check results')
+        logger.debug('batch_idx %s, titles.shape %s, articles.shape %s, article_lengths.shape %s, title_lengths.shape %s', batch_idx, titles.shape, articles.shape, article_lengths.shape, title_lengths.shape)
+        logger.debug('titles %s', titles)
+        logger.debug('title_lengths %s', title_lengths)
