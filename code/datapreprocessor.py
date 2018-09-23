f32eae390a84c50ac2177b5bd25b518b56e5f47b
bajarno <jarnoverhagen@gmail.com>
2018-09-24 00:25:47 +0200

    Fixed kaggleparser and preprocessor

--- code/datapreprocessor.py
+++ code/datapreprocessor.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from nltk import sent_tokenize, word_tokenize
 import csv
 
 class DataPreprocessor:
@@ -13,7 +12,7 @@ class DataPreprocessor:
             csvreader = csv.reader(f)
             for title, content in csvreader:
                 line = title + ' ' + content
-                words = self.tokenize(line)
+                words = line.split()
                 for word in words:
                     counter[word] += 1
 
@@ -25,17 +24,9 @@ class DataPreprocessor:
         words, counts = zip(*counter.most_common()[i_a:i_b])
         return words
 
-    def tokenize(self, text):
-        words = []
-        for sentence in sent_tokenize(text):
-            words.append(word_tokenize(sentence))
-
-        flat_words = [item for sublist in words for item in sublist] 
-        return flat_words
-
     def replace_unk(self, sentence, vocab):
         new_sentence = []
-        for word in self.tokenize(sentence):
+        for word in sentence.split():
             if word in vocab:
                 new_sentence.append(word)
             else:
@@ -49,7 +40,7 @@ class DataPreprocessor:
             for title, content in csvreader:
                 new_title = self.replace_unk(title, vocab)
                 new_content = self.replace_unk(content, vocab)
-                writer.writerow(['<s> ' + new_title + ' </s>', '<s> ' + new_content + ' </s>'])
+                writer.writerow([new_title, new_content])
                 
 
     def preprocess(self, input_file, output_file):
@@ -59,9 +50,10 @@ class DataPreprocessor:
 
 
 if __name__ == "__main__":
-    input_file = 'test/testdata.csv'
-    output_file = 'test/preprocessed_testdata.csv'
+    input_file = '../data/kaggle_parsed.csv'
+    output_file = '../data/kaggle_preprocessed_10000.csv'
 
     preprocessor = DataPreprocessor()
+    preprocessor.max_vocab_size = 10000
     preprocessor.preprocess(input_file, output_file)
 
--- code/kaggleparser.py
+++ code/kaggleparser.py
@@ -1,70 +1,77 @@
 import csv
 import re
+import sys
+from nltk import sent_tokenize, word_tokenize
+
+csv.field_size_limit(sys.maxsize)
 
 class KaggleParser:
     min_content_length = 10 # Minimum amount of words in content
-    max_content_length = 50 # Maximum amount of words in content
+    max_content_length = 100 # Maximum amount of words in content
+
+    tag_re = re.compile(r'<[^>]+>') # Regular expression for removing HTML tags
 
     def processTitle(self, title):
+        title = self.tag_re.sub('', title) # Remove HTML tags
 
         # Remove paper name from title
         title = title.replace(' - The New York Times', '')
+        title = title.replace(' - Breitbart', '')
 
         # Ignore Atlantic Daily articles due to noise
         if title.startswith('The Atlantic Daily:'):
             return None
 
-        return title
-
+        tokens = word_tokenize(title)
+        tokens = ['<s>'] + tokens + ['</s>']
+        return tokens
 
     def processContent(self, content):
-        sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', content)
+        content = self.tag_re.sub('', content) # Remove HTML tags
+        sentences = sent_tokenize(content)
 
-        maxindex = 0
-
+        tokens = []
         for s in sentences:
-            length = len(s.split())
+            s_tokens = ['<s>'] + word_tokenize(s) + ['</s>']
 
-            if maxindex + length < self.max_content_length:
-                maxindex += length
+            # Add sentence if total amount of tokens is lower than max
+            if len(tokens) + len(s_tokens) < self.max_content_length:
+                tokens += s_tokens
             else:
                 break
 
-        content = content.split()[:maxindex]
-
-        if len(content) < self.min_content_length:
+        if len(tokens) < self.min_content_length:
             return None
 
-        content = ' '.join(content)
-        return content
+        return tokens
 
-    def process_source(self, source_path, target_path):
-        source = open(source_path, 'r')
-        target = open(target_path, 'w')
-
+    def process_source(self, source, target):
         reader = csv.reader(source)
         writer = csv.writer(target)
 
-        i_r = 0
+        next(reader, None) # skip header
+
         for row in reader:
-            if i_r > 100:
-                break
+            title = self.processTitle(row[2])
+            content = self.processContent(row[9])
 
-            if i_r > 0:
-                title = self.processTitle(row[2])
-                content = self.processContent(row[9])
+            if title != None and content != None and len(title) < len(content):
+                writer.writerow([' '.join(title), ' '.join(content)])
 
-                if title != None and content != None:
-                    writer.writerow([title,content])
+    def process_sources(self, source_paths, target_path):
+        target = open(target_path, 'w')
 
-            i_r += 1
+        for source_path in source_paths:
+            source = open(source_path, 'r')
+
+            self.process_source(source, target)
 
 if __name__ == '__main__':
-    source_path = '../data/kaggle/articles2.csv'
+    source_paths = ['../data/kaggle/articles1.csv', '../data/kaggle/articles2.csv', '../data/kaggle/articles3.csv']
     target_path = '../data/kaggle_parsed.csv'
 
     parser = KaggleParser()
-    parser.process_source(source_path, target_path)
+    parser.process_sources(source_paths, target_path)
 
 
 
