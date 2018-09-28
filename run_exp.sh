b95591ad1e51654bb4bed101a317b1ca9af6f46f
Freek Boutkan <lgpu0012@test3.irc.sara.nl.irc.sara.nl>
2018-09-28 21:53:31 +0200

    Bugfixes surfsara

--- run_exp.sh
+++ run_exp.sh
@@ -8,17 +8,18 @@ module load cuDNN/7.0.5-CUDA-9.0.176
 module load OpenMPI/2.1.1-GCC-6.4.0-2.28
 export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.0.5-CUDA-9.0.176/lib64:/hpc/eb/Debian9/CUDA/9.0.176/lib64:$LD_LIBRARY_PATH
 
-cd ~
+cd ~/dl4nlt/code
 logdir="$TMPDIR"/$(date +"%Y-%m-%d_%H:%M:%S")/
-mkdir logdir
+mkdir $logdir
+touch $logdir/log
 
 
 echo 'Starting run' | tee -a "$logdir"/log
 pip install torch torchvision --user --no-cache -U
 echo 'Installed torch' | tee -a "$logdir"/log
 
-echo 'Start running python' | tee -a "$log"/log
-python -u train.py --output_dir $logdir 2>&1 | tee -a "$log"/log
-echo 'Done running python' | tee -a "$log"/log
+echo 'Start running python' | tee -a "$logdir"/log
+python -u train.py --output_dir $logdir 2>&1 | tee -a "$logdir"/log
+echo 'Done running python' | tee -a "$logdir"/log
 
 cp -r $logdir ~
