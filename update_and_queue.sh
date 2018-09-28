bcc737403e446355019d824a1f74419c4218b4cd
Freek <freek.boutkan@gmail.com>
2018-09-28 23:02:34 +0200

    Fix bug in git update shell script

--- update_and_queue.sh
+++ update_and_queue.sh
@@ -4,14 +4,15 @@ cd ~
 
 if [ ! -d "dl4nlt" ]; then
   git clone git@github.com:bajarno/dl4nlt.git
-  echo 'Cloned git' | tee -a $log
+  echo 'Cloned git'
 fi
 
 cd dl4nlt/code
 
 git fetch origin
 git reset --hard origin/master
-echo 'Reset to origin/master' | tee -a $log
+echo 'Reset to origin/master'
 
 cd ~/dl4nlt
 qsub -q run_exp.sh
+echo 'Registered job in queue'
